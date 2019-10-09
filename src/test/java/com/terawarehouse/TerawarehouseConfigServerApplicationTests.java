7214d34dfdee843562512980da11a2f1ce55baa3
Edward P. Legaspi <czetsuya@gmail.com>
2019-10-09 09:22:35 +0800

    Initial commit.

---
+++ src/test/java/com/terawarehouse/TerawarehouseConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terawarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerawarehouseConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
